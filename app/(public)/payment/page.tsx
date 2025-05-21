eba513077713788fb479c452b2eedacc336cfbb8
marArcz <mar19arcz@gmail.com>
2025-05-22 00:44:59 +0800

    removed use search param

--- app/(public)/payment/page.tsx
+++ app/(public)/payment/page.tsx
@@ -1,11 +1,7 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation'
-import React from 'react'
 
 const PaymentPage = () => {
-  const searchParams = useSearchParams()
-  const deliveryInfoId = searchParams.get('deliveryInfo')
   return (
     <div>
         Payment
